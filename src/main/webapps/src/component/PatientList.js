5324b387fb284e7ffbde0499b09852700dc5fe72
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2022-09-19 12:41:13 +0530

    Update PatientList.js

--- src/main/webapps/src/component/PatientList.js
+++ src/main/webapps/src/component/PatientList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap';
+
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 import { Link } from "react-router-dom";
