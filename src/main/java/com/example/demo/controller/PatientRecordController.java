184d1823094913f416f8352024eeb28a7935de50
Akshatparikh19 <88769364+Akshatparikh19@users.noreply.github.com>
2022-09-19 12:47:16 +0530

    Update PatientRecordController.java

--- src/main/java/com/example/demo/controller/PatientRecordController.java
+++ src/main/java/com/example/demo/controller/PatientRecordController.java
@@ -5,6 +5,7 @@ import com.example.demo.repositories.PatientRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 
